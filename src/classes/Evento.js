919c4e7a1d192a178b30904eb5553f24afb9ae5f
ItsJavito <javierenriqueos@gmail.com>
2022-09-04 01:40:18 -0500

    adicion de clases para eventos

---
+++ src/classes/Evento.js
@@ -0,0 +1,50 @@
+
+export class Evento{
+    constructor(nu_evnt, no_evnt, qt_pers, qt_hrs, desc_evnt, ubic, fh_inicio, fh_fin, url_evnt, fg_vig){
+        this.nu_evnt    = nu_evnt;
+        this.no_evnt    = no_evnt;
+        this.qt_pers    = qt_pers;
+        this.qt_hrs     = qt_hrs;
+        this.desc_evnt  = desc_evnt;
+        this.ubic       = ubic;
+        this.fh_inicio  = fh_inicio;
+        this.fh_fin     = fh_fin;
+        this.url_evnt   = url_evnt;
+        this.fg_vig     = fg_vig;
+    }
+
+    async update(){
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({   
+                NO_EVNT     : this.no_evnt, 
+                QT_PERS     : this.qt_pers,  
+                QT_HRS      : this.qt_hrs,   
+                DESC_EVNT   : this.desc_evnt,
+                UBIC        : this.ubic,     
+                FH_INICIO   : this.fh_inicio,
+                FH_FIN      : this.fh_fin,   
+                URL_EVNT    : this.url_evnt, 
+                FG_VIG      : this.fg_vig,
+            })
+        };
+        const response = await fetch(`https://genium-backend.herokuapp.com/eventos/${this.nu_evnt}`, requestOptions)
+        if(!response.ok){
+            throw new Error('No se pudo actualizar el evento')
+        }else{
+            return response.json()
+        }
+    }
+
+    // * Llamamos al controlador del lado del backend para que haga la eliminación del evento
+    async delete(){
+        const response = await fetch(`https://genium-backend.herokuapp.com/eventos/${this.nu_evnt}`, {
+            method: 'DELETE'
+        })
+        if (!response.ok) {
+            throw new Error('No se pudo eliminar el evento');
+        }
+    }
+}
+
---
+++ src/classes/GestorEventos.js
@@ -0,0 +1,65 @@
+import { Evento } from "./Evento.js";
+export class GestorEvento{
+    constructor(){}
+    /**
+     * * Funcion que traer todos los eventos de la base de datos
+     */
+    async obtenerEventos(){
+        const response = await fetch(`https://genium-backend.herokuapp.com/eventos`);
+        // response.ok devuelve true o false
+        // dependiendo del exito de la operacion
+        if (!response.ok) {
+            //lanzamos error si no se pudo
+            throw new Error('No se pudo obtener todos los eventos')
+        } else {
+            // devolvemos la consulta
+            const eventosJson = await response.json();
+            const eventos = eventosJson.map(evento => {
+                return(
+                    //constructor(nu_evnt, no_evnt, qt_pers, qt_hrs, desc_evnt, ubic, fh_inicio, fh_fin, url_evnt, fg_vig)
+                    new Evento(
+                        evento.NU_EVNT, 
+                        evento.NO_EVNT, 
+                        evento.QT_PERS, 
+                        evento.QT_HRS,
+                        evento.DESC_EVNT,
+                        evento.UBIC,
+                        evento.FH_INICIO,
+                        evento.FH_FIN,
+                        evento.URL_EVNT,
+                        evento.FG_VIG
+                    )
+                )
+            })
+            return eventos
+        }
+    }
+    // ! Función que buesca evento a través de su numero de evento
+    async ObtenerEvento(nu_evnt){
+        const response = await fetch(`https://genium-backend.herokuapp.com/eventos/${nu_evnt}`);
+        // response.ok devuelve true o false
+        // dependiendo del exito de la operacion
+        if (!response.ok) {
+            //lanzamos error si no se pudo
+            throw new Error('No se pudo obtener el evento')
+        } else {
+            // devolvemos la consulta
+            const eventoJson = await response.json();
+            const evento = 
+                    //constructor(nu_evnt, no_evnt, qt_pers, qt_hrs, desc_evnt, ubic, fh_inicio, fh_fin, url_evnt, fg_vig)
+                    new Evento(
+                        eventoJson.NO_EVNT, 
+                        eventoJson.NU_EVNT, 
+                        eventoJson.QT_PERS, 
+                        eventoJson.QT_HRS,
+                        eventoJson.DESC_EVNT,
+                        eventoJson.UBIC,
+                        eventoJson.FH_INICIO,
+                        eventoJson.FH_FIN,
+                        eventoJson.URL_EVNT,
+                        eventoJson.FG_VIG
+                    )
+            return evento
+        }
+    }
+}
