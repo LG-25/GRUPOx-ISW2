d5810e9c288365c05436f4e38366f40a27fd41c3
RicardoSilva <ricardosebastiand10s@gmail.com>
2022-09-05 01:01:37 -0500

    plantilla editar modifacada

--- src/components/Modaledit.component.jsx
+++ src/components/Modaledit.component.jsx
@@ -36,72 +36,65 @@ const Modaledit = () => {
         setTxtApMaterno(event.target.value);
       };
     //const butOnGuardar = () => {
-      //props.actualizarCliente(
-        //props.cliente.id,
-        //txtNombre,
-        //txtApPaterno,
-        //txtCorreo,
-        //txtTelefono,
-        //txtApMaterno
-      //);
-    //};
-    //class
-    return <div className="row container mt-5">
-        <div className="row justify-content-center">
-            <div className="col" />
-            <div className="col-md-5 table-responsive">
-                <div class="card" style={{ maxWidth: "1540px" }}>
-                    <div class="row g-0">
-                    <div className='mt-2 mb-2 ml-2 mr-2'>
-                            <label className="form-label">Nombre</label>
-                            <input
-                                className="form-control"
-                                type="text"
-                                defaultValue="Ricardo"
-                                onChange={txtApellidoOnChange}
-                            />
-                        </div>
-                        <div className='mt-2 mb-2 ml-2 mr-2'>
-                            <label className="form-label">Apellido Paterno</label>
-                            <input
-                                className="form-control"
-                                type="text"
-                                defaultValue="Ricardo"
-                                onChange={txtApellidoOnChange}
-                            />
-                        </div>
-                        <div className='mt-2 mb-2 ml-2 mr-2'>
-                            <label className="form-label">Apellido Materno</label>
-                            <input
-                                className="form-control"
-                                type="text"
-                                defaultValue="Ricardo"
-                                onChange={txtApellidoOnChange}
-                            />
-                        </div>
-                        <div className='mt-2 mb-2 ml-2 mr-2'>
-                            <label className="form-label">Correo</label>
-                            <input
-                                className="form-control"
-                                type="text"
-                                defaultValue="Ricardo"
-                                onChange={txtApellidoOnChange}
-                            />
-                        </div>
-                        <div className='mt-2 mb-2 ml-2 mr-2'>
-                            <label className="form-label">Telefono</label>
-                            <input
-                                className="form-control"
-                                type="text"
-                                defaultValue="Ricardo"
-                                onChange={txtApellidoOnChange}
-                            />
-                        </div>
-                    </div>
+  //props.actualizarCliente(
+  //props.cliente.id,
+  //txtNombre,
+  //txtApPaterno,
+  //txtCorreo,
+  //txtTelefono,
+  //txtApMaterno
+  //);
+  //};
+  //class
+  return <div className="row container mt-5">
+    <div className="row justify-content-center">
+      <div className="col" />
+      <div className="col-md-5 table-responsive">
+        <aside>
+          <div className="card bg-secondary text-black bg-opacity-25">
+            <div className="card-body">
+              <div className="row">
+                <div className="col">
+
                 </div>
+                <div className="col">
+                  <h2 className="text-center">Datos</h2>
+                </div>
+                <div className="col">
+
+                </div>
+              </div>
+              <div className="input-group-sm mb-2">
+                <label for="" className="form-label">Nombre</label>
+                <input type="text" defaultValue="Ricardo" className="form-control" onChange={txtApellidoOnChange}/>
+              </div>
+              <div className="input-group-sm mb-2">
+                <label for="" className="form-label">Apellido Paterno</label>
+                <input type="text" defaultValue="Ricardo" className="form-control" onChange={txtApellidoOnChange}/>
+              </div>
+              <div className="input-group-sm mb-2">
+                <label for="" className="form-label">Apellido Materno</label>
+                <input type="text" defaultValue="Ricardo" className="form-control" onChange={txtApellidoOnChange}/>
+              </div>
+              <div className="input-group-sm mb-2">
+                <label for="" className="form-label" >Correo</label>
+                <input type="text" defaultValue="Ricardo" className="form-control" onChange={txtApellidoOnChange}/>
+              </div>
+              <div className="input-group-sm mb-2">
+                <label for="" className="form-label" >Teléfono</label>
+                <input type="text" defaultValue="999999999" className="form-control" onChange={txtApellidoOnChange}/>
+              </div>
+              <div className="row mt-2 mb-2">
+                <div className="col">
+                  <button className="btn btn-primary" >Guardar</button>
+                </div>
+              </div>
             </div>
-            <div className="col" />
-        </div>
+          </div>
+        </aside>
+      </div>
+      <div className="col" />
     </div>
+  </div>
   };
   export default Modaledit;
--- src/views/EditarUsuario.jsx
+++ src/views/EditarUsuario.jsx
@@ -3,7 +3,6 @@ import Modaledit from "../components/Modaledit.component.jsx"
 const EditarUsuario=()=>{
     return (
         <div className="container">
-            <h1>Editar Datos</h1>
             <Modaledit></Modaledit>
         </div>
     )
