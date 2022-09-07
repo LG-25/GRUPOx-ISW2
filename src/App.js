ea8e345f5496eeb5371e4ad53f424536e6e797f6
Natalia <nati66819@gmail.com>
2022-09-07 16:01:06 -0500

    Actualizacion botones formulario

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import CatalogoEventos from './views/CatalogoEventos';
 import Principal from './views/Principal';
 import EditarUsuario from './views/EditarUsuario';
 import SignUpUsuario from './views/SignUpUsuario';
-import RegistroEvento from './views/RegistroEvento';
 import SignupEntidad from './views/SignupEntidad';
 import Evento_Detalles from './views/Evento_Detalles';
 // ! Comentar este componente, solamente es para pruebas
@@ -25,7 +24,6 @@ function App() {
           <Route path = "/evento" component={Evento_Detalles}></Route>
           <Route path = "/editarUsuario" component={EditarUsuario}></Route>
           <Route path = "/SignUpUsuario" component={SignUpUsuario}/>
-          <Route path = "/RegistroEvento" component={RegistroEvento}></Route>
           <Route path="/SignupEntidad" component={SignupEntidad}></Route>
           <Route path = "/catalogoEventos"></Route>
           <Route path = "/evento" component={Evento_Detalles}></Route>
