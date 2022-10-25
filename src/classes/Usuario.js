80779ddf61b870c4a7f2fc3411364eca5cbf89b9
007mastercito <fabrifillo@gmail.com>
2022-10-25 18:52:06 -0500

    modificado clase usuario

--- src/classes/Usuario.js
+++ src/classes/Usuario.js
@@ -1,5 +1,5 @@
-class Usuario{
-    constructor(co_usr_inv, nom_usr, correo, contra, nom, ap_pat, ap_mat, fh_nacimiento, fh_creacion){
+export class Usuario{
+    constructor(co_usr_inv, nom_usr, correo, contra, nom, fh_creacion, pais, ruc){
         this.co_usr_inv= co_usr_inv
         this.nom_usr= nom_usr
         this.correo = correo
@@ -9,5 +9,8 @@ class Usuario{
         this.ap_mat= ap_mat
         this.fh_nacimiento= fh_nacimiento
         this.fh_creacion= fh_creacion
+        this.fh_creacion= fh_creacion
+        this.pais = pais
+        this.ruc = ruc
     }
 }
