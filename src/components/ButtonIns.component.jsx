aa8f2346900591978fb3eb19ffd6ae128bd7d4ba
RicardoSilva <ricardosebastiand10s@gmail.com>
2022-10-25 12:59:51 -0500

    CatalogoEventos mejorado

--- src/components/ButtonIns.component.jsx
+++ src/components/ButtonIns.component.jsx
@@ -1,11 +1,8 @@
 const ButtonIns=(props)=>{
     const butOnClick=()=>{
         props.onRegis()
-<<<<<<< HEAD
         //navegar -> (link)
-=======
         //navegar(link)
->>>>>>> 2d3491d (Plantilla detalles agregada)
     }
     return <button id="" className={props.color} onClick={butOnClick} >{props.nombre}</button>
 }
