9ec742eb6728e2c7f6202fce0ca4950ceb8dd84d
LG-25 <liasandoval2001@gmail.com>
2022-10-26 13:41:50 -0500

    Login

--- src/views/loginUsuario.jsx
+++ src/views/loginUsuario.jsx
@@ -3,32 +3,39 @@ import { Link } from 'react-router-dom';
 
 const LoginUsuario = ()=> {
 
-    const [nomUSR, setnomUSR] = useState("");
+    //usuario invitado
+    const [correoUSR, setcorreoUSR] = useState("");
     const [contra, setContra] = useState("");
-    const [nomUSRI, setnomUSRI] = useState("");
-    const [contraI, setContraI] = useState("");
+    //usuario entidad
+    const [correoUSREN, setcorreoUSREN] = useState("");
+    const [contraEN, setContraEN] = useState("");
 
 
-    const validarUsuario = async () =>{
-        const response = await fetch(`https://genium-backend.herokuapp.com/usuarios/${nomUSR}/${contra}`); 
+    const validarUsuarioEN = async () =>{
+        //usuario entidad
+        const response = await fetch(`https://genium-backend.herokuapp.com/login?correo=${correoUSREN}&contra=${contraEN}`); 
+        //https://genium-backend.herokuapp.com/login?correo=javierenriqueos@gmail.com&contra=admi123
         const res = await response.json();
 
-        const USRvalido =res.respuesta.Usuario.length >0;
+        const USRvalido = res["CO_USR"] != undefined;
         if(USRvalido){
-            window.location.href='/principalUsuario';
+            window.location.href='/InicioEntidad';
+            sessionStorage.setItem('userEN' , `${res["CO_USR"]}`)
         }else{
             alert("Credenciales incorrectas")
         }
 
     }
 
-    const validarUsuarioI = async () =>{
-        const response = await fetch(`https://genium-backend.herokuapp.com/usuariosI/${nomUSRI}/${contraI}`); 
+    const validarUsuario = async () =>{
+        //usuario invitado
+        const response = await fetch(`https://genium-backend.herokuapp.com/login?correo=${correoUSR}&contra=${contra}`); 
         const res = await response.json();
 
-        const USRvalido =res.respuesta.Usuario.length >0;
+        const USRvalido = res["CO_USR_INVT"] != undefined;
         if(USRvalido){
-            window.location.href='/principalUsuarioI';
+            window.location.href='/InicioUsuario';
+            sessionStorage.setItem('user' , `${res["CO_USR_INVT"]}`);
         }else{
             alert("Credenciales incorrectas")
         }
@@ -38,20 +45,48 @@ const LoginUsuario = ()=> {
 
     
 return(
-    <div className="container fluid my-4 w-50 border p-5 position-absolute top-50 start-50 translate-middle">
+<div>
+    <div className="container fluid my-4 w-75 p-5 position-absolute top-50 start-50 translate-middle">
         <div className="row">
-            <div className="col">
-                <h2 className="mb-4 text-center">¡Bienvenido usuario!</h2>
+            
+            <div className="col-6 border p-5">
+                    <h2 className="mb-4 text-center">¡Bienvenido usuario entidad!</h2>
+                    <div className="">
+                        <label for="floatingInput">Correo electrónico</label>
+                        <input type="text" className="form-control"  placeholder="Correo electrónico" onChange={e => setcorreoUSREN(e.target.value)} value={correoUSREN}/>
+                    </div>
+                    <div className="my-4">
+                        <label for="floatingInput">Contraseña</label>
+                        <input type="text" className="form-control"  placeholder="Contraseña" onChange={e => setContraEN(e.target.value)} value={contraEN}/>
+                    </div>
+                    <div className="my-4 text-center">
+                        <button className="btn btn-success" onClick={validarUsuarioEN}>Ingresar</button>
+                    </div>
+                    <div className="text-center">
+                        <Link to="/">
+                            <div className="fw-4 text-center">¿No tienes cuenta? Regístrate</div>
+                        </Link>
+                    </div>
+                    <div className="text-center">
+                        <Link to="/">
+                            <div className="fw-4 text-center">Volver</div>
+                        </Link>
+                    </div>
+
+            </div>
+
+            <div className="col-6 border p-5">
+                <h2 className="mb-4 text-center">¡Bienvenido usuario invitado!</h2>
                 <div className="">
-                    <label for="floatingInput">Nombre de usuario</label>
-                    <input type="text" className="form-control"  placeholder="Usuario" onChange={e => setnomUSR(e.target.value)} value={nomUSR}/>
+                    <label for="floatingInput">Correo electrónico</label>
+                    <input type="text" className="form-control"  placeholder="Correo electrónico" onChange={e => setcorreoUSR(e.target.value)} value={correoUSR}/>
                 </div>
                 <div className="my-4">
                     <label for="floatingInput">Contraseña</label>
-                    <input type="text" className="form-control"  placeholder="Contrasena" onChange={e => setContra(e.target.value)} value={contra}/>
+                    <input type="text" className="form-control"  placeholder="Contraseña" onChange={e => setContra(e.target.value)} value={contra}/>
                 </div>
                 <div className="my-4 text-center">
-                    <button className="btn btn-success">Ingresar</button>
+                    <button className="btn btn-success" onClick={validarUsuario}>Ingresar</button>
                 </div>
                 <div className="text-center">
                     <Link to="/">
@@ -68,6 +103,7 @@ return(
             
         </div>
     </div> 
+</div>
 )
 }
 export default LoginUsuario
