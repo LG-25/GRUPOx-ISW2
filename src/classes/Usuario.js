6107bc2c20b075dadbaff403ea0e32a2dfcc2501
007mastercito <fabrifillo@gmail.com>
2022-10-25 20:35:29 -0500

    Se agrego el metodo obtener eventos de una entidad y se arreglo la clase usuario

--- src/classes/Usuario.js
+++ src/classes/Usuario.js
@@ -1,14 +1,10 @@
 export class Usuario{
-    constructor(co_usr_inv, nom_usr, correo, contra, nom, fh_creacion, pais, ruc){
-        this.co_usr_inv= co_usr_inv
+    constructor(co_usr, nom_usr, correo, contra,fh_nacimiento, fh_creacion, pais, ruc){
+        this.co_usr= co_usr
         this.nom_usr= nom_usr
         this.correo = correo
         this.contra= contra
-        this.nom= nom
-        this.ap_pat= ap_pat
-        this.ap_mat= ap_mat
         this.fh_nacimiento= fh_nacimiento
-        this.fh_creacion= fh_creacion
         this.fh_creacion= fh_creacion
         this.pais = pais
         this.ruc = ruc
