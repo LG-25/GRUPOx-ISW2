e7a6c9a725c1b3d0f7c2cf425f737049f5acdd26
007mastercito <fabrifillo@gmail.com>
2022-11-27 14:45:43 -0500

    añadir tachito

--- src/components/CartaEvento.jsx
+++ src/components/CartaEvento.jsx
@@ -89,9 +89,19 @@ export const CartaEvento = (props) => {
           <p className="card-text">Fecha Inicio: {dayI.toLocaleDateString()}</p>
           <p className="card-text">Fecha Fin: {dayF.toLocaleDateString()}</p>
           {evento.co_estd === 1 && <><a href="#" className="btn btn-primary" onClick={butonClick1}>Confirmar Asistencia</a>
-          <a href="#" className="btn btn-primary mt-2" onClick={butOnClick3}>Eliminar</a></>}
+          <div className='mt-2'><button type="button" className="btn btn-danger" onClick={butOnClick3}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+            <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+            </svg> ELIMINAR
+          </button></div></>}
           {evento.co_estd === 2 && <><a href="#" className="btn btn-primary" onClick={butOnClick2}>Ver QR</a>
-          <a href="#" className="btn btn-primary mt-2" onClick={butOnClick3}>Eliminar</a></>}
+          <div className='mt-2'><button type="button" className="btn btn-danger" onClick={butOnClick3}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+            <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+            </svg> ELIMINAR
+          </button></div></>}
           {evento.co_estd === 3 && <a href="#" className="btn btn-primary" onClick={butonClick3}>Mostrar Certificado</a>}
         </div>
           <ModalQr nu_evnt={evento.nu_evnt} mostrar={debeMostrarModal} ocultar={onModalClose}></ModalQr>
