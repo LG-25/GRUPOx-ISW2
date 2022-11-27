d749890a79467d9e881486494c435180ad6c37aa
007mastercito <fabrifillo@gmail.com>
2022-11-27 17:03:38 -0500

    arreglo importaciones innecesarias

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,8 @@ import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+//import ListItemIcon from '@mui/material/ListItemIcon';
+//import ListItemText from '@mui/material/ListItemText';
 
 
 export function Sidebar() {
